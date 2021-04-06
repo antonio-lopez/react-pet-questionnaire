2ef6357240c0a2b030f7b230b6662b8a25ee6de3
Antonio Lopez <36496887+antonio-lopez@users.noreply.github.com>
2021-04-06 14:51:31 -0700

    quiz questions and results displayed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import { useState, useEffect } from 'react';
-import './App.css';
-import Question from './components/Question';
 import Quiz from './components/Quiz';
 import quizQuestions from './api/quizQuestions';
+import Result from './components/Result';
+import './App.css';
 
 function App() {
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(1);
   const [questionId, setQuestionId] = useState(1);
   const [question, setQuestion] = useState('');
   const [answerOptions, setAnswerOptions] = useState([]);
   const [answer, setAnswer] = useState('');
   const [answersCount, setAnswersCount] = useState({});
   const [result, setResult] = useState('');
+
   // const [quizValues, setQuizValues] = useState({
   //   counter: 0,
   //   questionId: 1,
@@ -54,14 +55,54 @@ function App() {
     return array;
   };
 
-  const handleAnswerSelected = (event) => {};
+  const handleAnswerSelected = (event) => {
+    console.log('CONSOLE EVENT TRGT:', event.currentTarget.value);
+    setUserAnswer(event.currentTarget.value);
+    if (questionId < quizQuestions.length) {
+      // setTimeout(() => setNextQuestion(), 300);
+      setNextQuestion();
+    } else {
+      // setTimeout(() => setResults(getResults()), 300);
+      setResults(getResults());
+    }
+  };
 
-  console.log(question);
-  console.log(answerOptions);
+  const setUserAnswer = (answer) => {
+    setAnswersCount({
+      ...answersCount,
+      [answer]: (answersCount[answer] || 0) + 1,
+    });
+    setAnswer(answer);
+  };
 
-  return (
-    <div className='app'>
-      <h2>Pet Quiz</h2>
+  const setNextQuestion = () => {
+    setCounter(counter + 1);
+    setQuestionId(questionId + 1);
+    setQuestion(quizQuestions[counter].question);
+    setAnswerOptions(quizQuestions[counter].answers);
+    setAnswer('');
+  };
+
+  const getResults = () => {
+    const answersCountKeys = Object.keys(answersCount);
+    const answersCountValues = answersCountKeys.map((key) => answersCount[key]);
+    const maxAnswersCount = Math.max.apply(null, answersCountValues);
+
+    return answersCountKeys.filter(
+      (key) => answersCount[key] === maxAnswersCount
+    );
+  };
+
+  const setResults = (result) => {
+    if (result.length === 1) {
+      setResult(result[0]);
+    } else {
+      setResult('Undetermined');
+    }
+  };
+
+  const renderQuiz = () => {
+    return (
       <Quiz
         answer={answer}
         answerOptions={answerOptions}
@@ -70,6 +111,25 @@ function App() {
         questionTotal={quizQuestions.length}
         onAnswerSelected={handleAnswerSelected}
       />
+    );
+  };
+
+  const renderResult = () => {
+    return <Result quizResult={result} />;
+  };
+
+  // console.log('counter:', counter);
+  // console.log('questionID', questionId);
+  // console.log('question:', question);
+  // console.log('answerOptions:', answerOptions);
+  // console.log('answer:', answer);
+  // console.log('answersCount:', answersCount);
+  // console.log('result:', result);
+
+  return (
+    <div className='app'>
+      <h2>Pet Quiz</h2>
+      {result ? renderResult() : renderQuiz()}
     </div>
   );
 }
---
+++ src/components/Result.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Result({ quizResult }) {
+  return <div className='result'>You prefer {quizResult}!</div>;
+}
+
+export default Result;
